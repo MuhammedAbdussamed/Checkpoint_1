f5b57711ea55cfe6b20403d5ab9b9763cfdeef0c
MuhammedAbdussamed <muhammedabdussamed2006@gmail.com>
2025-08-07 23:39:44 +0300

    Version 1.2

--- Assets/Scripts/PlayerScript/PlayerController.cs
+++ Assets/Scripts/PlayerScript/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     {
         _playerMovement.Enable();    // Inputları aktif ettik.
         _playerData._isFacingRight = true;    // Oyun başladığında karakter sağa dönük olduğu için true dedik
+        _playerData._mono = this;    // Monobehaviour referansın bu script yap.
     }
 
     private void Update()
@@ -24,28 +26,70 @@ public class PlayerController : MonoBehaviour
         Flip();
         JumpStateTrigger();
         Attack();
-        Dash();
+        DashStateTrigger();
 
         /* Inputs */
         _moveDirection = _playerMovement.FindActionMap("Movement").FindAction("Move").ReadValue<Vector2>();  // Input değerini _moveDirection'a ata.
 
         /* PlayerData Assign */
-        _playerData._playerMoveDirection = _moveDirection;    // Bütün datayı tutan PlayerProperties'e moveDirection'u geçiriyoruz.
-        _playerData._isWalking = _moveDirection.x != 0;     // isWalking değişkenini hız 0 dan farklıysa true yap.
-        _playerData._jumpDirection = _jumpDirection;        // PlayerProperties'de ki jumpDirection değerini PlayerControllerda ki jumpDirection değeriyle aynı yaptık.
+        _playerData._playerMoveDirection = _moveDirection;      // Bütün datayı tutan PlayerProperties'e moveDirection'u geçiriyoruz.
+        _playerData._isWalking = _moveDirection.x != 0;         // isWalking değişkenini hız 0 dan farklıysa true yap.
+        _playerData._jumpDirection = _jumpDirection;            // PlayerProperties'de ki jumpDirection değerini PlayerControllerda ki jumpDirection değeriyle aynı yaptık.
+        gameObject.layer = _playerData._playerLayer;            // Player Properties'de ki layer değerini objeye yansıt.
     }
 
     void OnEnable()
     {
         _playerMovement.FindActionMap("Jump").FindAction("Jumping").performed += ctx => _playerData._jumpPressed = true;    // Gelen Input ile _jumpPressed değişkenini true çevir.Bu bir event aboneliğidir.
+        _playerMovement.FindActionMap("Movement").FindAction("Dash").performed += ctx => _playerData._dashPressed = true;   // Gelen Input ile _dashPressed değişkenini true çevir.Bu bir event aboneliğidir.
     }
 
     void OnDisable()
     {
         _playerMovement.FindActionMap("Jump").FindAction("Jumping").performed -= ctx => _playerData._jumpPressed = true;    // Script devre dışı kalırsa aboneliği kaldır.
+        _playerMovement.FindActionMap("Movement").FindAction("Dash").performed -= ctx => _playerData._dashPressed = false;  // Script devre dışı kalırsa aboneliği kaldır  
     }
 
+
+    #region StateTransition
+
+    private void JumpStateTrigger()
+    {
+        if (_playerData._isGrounded && _playerData._jumpPressed)    // Karakter yerde ise ve zıplama tuşuna basılmışsa.
+        {
+            _playerData._isJumping = true;                          // Karakter zıplıyor değişkenini true yap.
+            _playerData._jumpPressed = false;                       // Tuşa basıldı mı değişkenini false yap ki döngü tekrar tekrar oynamasın.
+        }
+    }
+
+    private void DashStateTrigger()
+    {
+        if (_playerData._dashPressed)                               // Dash her durumda atılabileceği için eğer dash tuşuna basılmışsa...
+        {
+            _playerData._isDashing = true;                          // Karakter atılıyor mu true çevir.
+            _playerData._dashPressed = false;                       // Atılma tuşuna basıldı mı false çevir ki döngü tekrarlanmasın.
+        }
+    }
+
+    #endregion
+
     #region Functions
+
+    public void Run(PlayerProperties _playerData)
+    {
+        Vector2 newVelocity = new Vector2(_playerData._playerMoveDirection.x * _playerData._speed, _playerData._rb.linearVelocity.y);
+        _playerData._rb.linearVelocity = newVelocity;
+        if (_playerData._playerMoveDirection.x != 0f)
+        {
+            _playerData._animator.SetBool("isWalking", true);
+            _playerData._isWalking = true;
+        }
+        else
+        {
+            _playerData._animator.SetBool("isWalking", false);
+            _playerData._isWalking = false;
+        }
+    }
 
     private void Flip()
     {
@@ -67,33 +111,26 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void JumpStateTrigger()
-    {
-        if (_playerData._isGrounded && _playerData._jumpPressed)    // Karakter yerde ise ve zıplama tuşuna basılmışsa.
-        {
-            _playerData._isJumping = true;                          // Karakter zıplıyor değişkenini true yap.
-            _playerData._jumpPressed = false;                       // Tuşa basıldı mı değişkenini false yap ki döngü tekrar tekrar oynamasın.
-        }
-    }
-
     private void Attack()
     {
         if (_playerMovement.FindActionMap("Attack").FindAction("Attack1").triggered)    //  Bu input trigger olduğunda devam et.
         {
             _playerData._animator.SetTrigger("Attack1");             // Attack1 animasyonunu triggerla . ( Bir kere çalışmasına sebep olur )
+            Vector2 dashDirection = _playerData._isFacingRight ? Vector2.right : Vector2.left;
+            _playerData._rb.linearVelocity = dashDirection * _playerData._dashSpeed;
+            StartCoroutine(ResetAttack2(_playerData));
         }
 
         if (_playerMovement.FindActionMap("Attack").FindAction("Attack2").triggered)    //  Bu input trigger olduğunda devam et.
         {
-            _playerData._animator.SetTrigger("Attack2");            // Attack2 animasyonunu triggerla . ( Bir kere çalışmasına sebep olur )
+            _playerData._animator.SetTrigger("Attack2");            // Attack2 animasyonunu triggerla . ( Bir kere çalışmasına sebep olur         
         }
     }
 
-    private void Dash()
+    IEnumerator ResetAttack2(PlayerProperties _playerData)
     {
-        if (_playerMovement.FindActionMap("Movement").FindAction("Dash").triggered)
-        {
-        }
+        yield return new WaitForSeconds(_playerData._dashTime);
+        _playerData._rb.linearVelocity = Vector2.zero;
     }
 
     #endregion
--- Assets/Scripts/PlayerScript/PlayerProperties.cs
+++ Assets/Scripts/PlayerScript/PlayerProperties.cs
@@ -10,27 +10,35 @@ public class PlayerProperties : MonoBehaviour
     public float _speed;            // Karakterin hızı.
     public float _jumpPower;        // Karakterin zıplama gücü.
 
+    public MonoBehaviour _mono;     // State scriptlerinde Monobehaviour'un özelliklerini kullanabilmek için referans.
+    public PlayerController _playerController;  // PlayerController referansı . Bu sayede diğer scriptlerden playerControllera ulaşabiliriz.
+
     public IState currentState;     // Şimdi ki durum.
 
     [Header("Components")]
     public Animator _animator;
     public Rigidbody2D _rb;
+    public Transform _transform;
+    public int _playerLayer;
 
     [Header("Bools")]
-    public bool _isGrounded;
-    public bool _isWalking;
+    public bool _isGrounded;        // Karakter yerde mi ?
+    public bool _isWalking;         // Karakter yürüyor mu ?
     public bool _isFacingRight;     // Karakterin dönmüş olduğu yönü temsil eder.
     
-    public bool _isJumping;
-    public bool _jumpPressed;
+    public bool _isJumping;         // Karakter zıplıyor mu?
+    public bool _jumpPressed;       // Zıplama tuşuna basıldı mı?
 
-    public bool _isDashing;
-    public bool _dashPressed;
-
+    public bool _isDashing;         // Karakter atılıyor mu?
+    public bool _dashPressed;       // Atılma tuşuna basıldı mı?
+    public float _dashTime;         // Atılma süresi.
+    public int _dashSpeed;          // Atılma hızı.
+    
     [Header("States")]
     public IState _idleState;       // Idle durumu
     public IState _walkState;       // Yürüme durumu
     public IState _jumpState;       // Zıplama durumu
+    public IState _dashState;       // Atılma durumu
 
     [Header("Variables")]
     public Vector2 _playerMoveDirection;
@@ -52,6 +60,7 @@ public class PlayerProperties : MonoBehaviour
         _idleState = new Idle_State();          // Idle durumunun atamasını yaptık.
         _walkState = new Walk_State();          // Walk durumunun atamasını yaptık.
         _jumpState = new JumpState();           // Jump durumunun atamasını yaptık.
+        _dashState = new Dash_State();          // Atılma durumunun atamasını yaptık.
     }
 
     void Update()
